c5e16b042e59411619a5c6d0104c7822e07383d9
Jon <jon@Jons-MacBook-Air.local>
2026-06-29 02:32:53 -0400

    Fix

--- src/components/InsightsPage/InsightsSection.tsx
+++
@@ -1,5 +0,0 @@
-import InsightsPage from './InsightsPage';
-
-export default function InsightsSection() {
-  return <InsightsPage />;
-}
--- src/components/InsightsPage/insightsDisplay.ts
+++
@@ -1,1 +0,0 @@
-export const formatInsightsValue = (value: number): string => String(value);
