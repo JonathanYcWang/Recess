bf19d565284987a8cf1ce773070b19fe12b3f256
JonathanYcWang <jon@Jons-MacBook-Air.local>
2026-07-30 11:09:07 -0400

    FIx format

--- src/UI/Redux/Selectors/Quiz/quizSelectors.ts
+++ src/UI/Redux/Selectors/Quiz/quizSelectors.ts
@@ -2,9 +2,10 @@ import { createSelector } from '@reduxjs/toolkit';
 import type { QuizValue } from '@/Shared/Types/AppState';
 import type { RootState } from '../../../Redux/store';
 
-const selectQuizState = (_state: RootState): QuizValue =>
+const selectQuizState = (state: RootState): QuizValue => {
+  void state;
   // state.appState?.quiz ??
-  ({
+  return {
     currentQuestionId: 'Q1',
     selectedChoices: [],
     isComplete: false,
@@ -12,7 +13,8 @@ const selectQuizState = (_state: RootState): QuizValue =>
       mbti: 'INTJ',
       dominantFriction: ['distraction', 'starting'],
     },
-  });
+  };
+};
 
 const selectCurrentQuestionId = createSelector([selectQuizState], (quiz) => quiz.currentQuestionId);
 
